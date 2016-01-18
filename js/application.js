0d39d48c111d16ea5cd98ffdb9c20d9bfce5c6ff
Aleksandar Stojadinovic <astojadinovicsd@gmail.com>
2016-01-18 19:15:55 +0100

    Navigation - scrolling and smooth animation

--- js/application.js
+++ js/application.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
 
+	function smoothAnimation (e) {
+		   // prevent default anchor click behavior
+	   e.preventDefault();
+
+	   // store hash
+	   var hash = this.hash;
+
+	   // animate
+	   $('html, body').animate({
+	       scrollTop: $(hash).offset().top
+	     }, 1000, function(){
+
+	       // when done, add hash to url
+	       // (default click behaviour)
+	       window.location.hash = hash;
+	     });
+	}
+
 	
 
-			
+	$(".nav li a[href^='#']").on('click', smoothAnimation );
+				
 	
 });
 
